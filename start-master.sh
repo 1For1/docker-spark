90d8c77744062e2fa0614d891591bb158f0c8245
Al Brown <al@1for.one>
2016-03-22 19:36:19 -0700

    Updated mesos and spark

--- start-master.sh
+++ start-master.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker pull meyerson/docker-spark
-docker run -d -t -P --name spark_master 1for1/docker-spark /start-master.sh "$@"
+docker run -d -t -P --name spark_master 1for1/docker-spark /app/start-master.sh "$@"
